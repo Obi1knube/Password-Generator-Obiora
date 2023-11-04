d6d4780a8575101698afb689f5ed6d9fc28d1f17
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2023-11-04 09:37:32 +0000

    Corrections

--- script.js
+++ script.js
@@ -1,71 +1,87 @@
 //Generate Passwords
-function generatePassword(userLength,  includeLowercaseChars,includeUppercaseChars,includeNumericChars,includeSpecialChars ){
-var includeLowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var includeUppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var includeNumericChars = "0123456789";
-var includeSpecialChars = "!@#$%^&*()_+~|}{[]:;?><./-,=";
 
-var password="";
-var availableChars=[];
+var includeLowercaseChars = ['a','b','c','d','e','f','g','h'];
+var includeUppercaseChars = ['A','B','C','D','E','F','G','H'];
+var includeNumericChars = ['0','1','2','3','4','5','6','7','8','9'];
+var includeSpecialChars = ['&','!','@','#','$'];
 
-// 1/ ask me what length password I want
-    // use prompt to get password length
+//var password="";
+//var availableChars=[];
 
-   var userLength = prompt('What is the length of your password?')
 
-      // check to make sure number is between 8-128 
-    if (userLength < 8 || userLength > 128){
-      alert('Please use a length between 8 and 128')
-      return(userLength);
-    }
-    console.log(userLength);
+function userChoice(){
+  var userLength= parseInt(prompt(" choose betweem 8 and 128 characters to generate password" ),10);
+  console.log(typeof(userLength));
+  if (userLength < 8 ){
+       alert('Please use a length between 8 and 128')  
+        return null; 
+  }
+  if (userLength > 128 ){
+       alert('Please use a length between 8 and 128')  
+        return null; 
+  }
+ if ( Number.isNaN(userLength)){
+    alert("please introduce a number")
+    return null;
 
-        //** */ 2/ do you want special characters?
- 
-  var text = "Include Special characters!\n Either OK or Cancel." 
-  if (confirm(text)===true){
-    availableChars+=includeSpecialChars;
-    }else{
-      availableChars=availableChars;
-          }
-          console.log(availableChars);
-        
-       //**/ 3/ do you want numbers?
-  var text = "Include Numbers!\n Either OK or Cancel." 
-  if (confirm(text)===true){
-    availableChars+=includeNumericChars;
-    }else{
-      availableChars=availableChars;
-    }
-    console.log(availableChars);
+ }
+ var hasSpecialChars= confirm("do you want to include special Characters?")
+ console.log(hasSpecialChars);
+ var hasNumericChars= confirm("do you want to include Numbers?");
+ var hasUpercaseLetters=confirm("Do you want to include Uppercase Letters?");
+ var hasLowercaseletters =confirm ("do you want to include Lowercase letters?");
 
-        //**/ 4/ do you want uppercase
-      var text = "Include Uppercase Letters!\n Either OK or Cancel." 
-  if (confirm(text)===true){
-    availableChars+=includeUppercaseChars;
-    }else{
-    availableChars=availableChars;
-          }
-          console.log(availableChars);
+ //If everthing is false, do not proceed
+ if (hasSpecialChars=== false && hasNumericChars===false &&hasUpercaseLetters ===false && hasLowercaseletters ===false){
+  alert("please choose a character"); 
+  return null;
 
-   //** */ 5/ lowercase letters
-   var text = "Include Lowercase Letters!\n Either OK or Cancel." 
-   if (confirm(text)===true){
-     availableChars+=includeLowercaseChars;
-     }else{
-     availableChars=availableChars;
-          }
-          console.log(availableChars);
-   
-      // ** */6/ generate password
-    
-        // create array that holds users preferences 
-     
-    }
-     
+ }
+//object is used to return user password length and other choices made by user 
+var obj={
+  userLength, 
+  hasSpecialChars,
+  hasNumericChars,
+  hasUpercaseLetters,
+  hasLowercaseletters
+};
+return obj;
 
-       // **7/ display it on html provided
-  
+}
+//Function UserChoice  is made available to the funtion generate password  
+function generatePassword(){
+  var choices= userChoice();
+  if (!choices) {
+    return null;
+  }
+  var possibleChars=[];
+  var results=[];
+
+  if(choices.hasSpecialChars){                         //if hasSpecialchsrs=true
+console.log('User confirmed special character');
+
+    possibleChars= possibleChars.concat(includeSpecialChars);
+   }
+
+   if (choices.hasNumericChars){
+    possibleChars= possibleChars.concat(includeNumericChars);
+   }
+   if (choices.hasUpercaseLetters) {
+    possibleChars = possibleChars.concat(includeUppercaseChars);
+  }
+
+  if (choices.hasLowercaseletters) {
+    possibleChars = possibleChars.concat(includeLowercaseChars);
+  }
+  for (var i = 0; i < choices.userLength; i++) {
+    var randomIndex = Math.floor(Math.random() * possibleChars.length);
+    results.push(possibleChars[randomIndex]);
+  }
+    console.log(results);
+  return results.join('');
+}
+
+
   var generateBtn = document.querySelector("#generate");
   
   // Write password to the #password input
